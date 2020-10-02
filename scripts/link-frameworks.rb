af4efb9f783fa18cc768512d19624deedb5dcf5f
Andrew Childs <andrew.childs@bibo.com.ph>
2020-10-02 16:22:12 +0900

    [wip] more rewriting

--- scripts/link-frameworks.rb
+++ scripts/link-frameworks.rb
@@ -3,14 +3,24 @@
 require 'psych'
 
 def framework_path(install_name)
+  return install_name if install_name.start_with?("@")
+
   case
   when (m = %r{/System/Library/(?:|Private)Frameworks/([^/]+).framework/(.+)}.match(install_name))
     framework_name, suffix = m.captures
     "@#{framework_name}@/Library/Frameworks/#{framework_name}.framework/#{suffix}"
-  when "/usr/lib/libobjc.A.dylib"
+  when install_name == "/usr/lib/libobjc.A.dylib"
     "@libobjc@/lib/libobjc.A.dylib"
+  when install_name == "/usr/lib/libcharset.1.dylib"
+    "@libcharset@/lib/libcharset.1.dylib"
+  when install_name == "/usr/lib/libnetwork.dylib"
+    "@libnetwork@/lib/libnetwork.dylib"
+  when (m = %r{/usr/lib/system/(.+)}.match(install_name))
+    lib, = m.captures
+    "@Libsystem@/lib/system/#{lib}"
   else
-    raise "Unhandled re-export path: #{install_name}"
+    warn "Unhandled re-export path: #{install_name}"
+    install_name
   end
 end
 
@@ -26,6 +36,20 @@ def rewrite(fpath)
         changed = true
         v.children.each do |re_export|
           re_export.value = framework_path(re_export.value)
+        end
+      end
+
+      if k.value == 'reexported-libraries'
+        v.children.each do |re_export|
+          re_export.children.each_slice(2) do |ck, cv|
+            if ck.value == 'libraries'
+              cv.children.each do |lib|
+                old_value = lib.value
+                lib.value = framework_path(lib.value)
+                changed ||= old_value != lib.value
+              end
+            end
+          end
         end
       end
     end
